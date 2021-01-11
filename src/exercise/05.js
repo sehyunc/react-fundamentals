30851e6b3db306897f70a7b6b82f1da5355e93c9
Sehyun Chung <sehyun@berkeley.edu>
2021-01-11 01:20:26 -0500

    complete ex5 ec 1

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -37,12 +37,23 @@ const largeBox = (
   </div>
 )
 
+const Box = ({className, style, ...otherProps}) => (
+  <div className={`box ${className}`} style={style} {...otherProps}></div>
+)
+
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+      <Box className="box--medium" style={{backgroundColor: 'pink'}}>
+        medium pink box
+      </Box>
+      <Box className="box--large" style={{backgroundColor: 'orange'}}>
+        large orange box
+      </Box>
+      <Box>sizeless box</Box>
     </div>
   )
 }
